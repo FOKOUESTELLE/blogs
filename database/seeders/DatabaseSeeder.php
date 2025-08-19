0209080e0b040c5620216c471a79b832b379a861
SomPascal <somfuncky8@gmail.com>
2025-08-19 13:53:48 +0100

    deleted useless space in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            
             CategorySeeder::class,
             PostSeeder::class,
             UserSeeder::class,
-            
         ]);
     }
 }
